caf4d6cea09789a374471463d2473055c5e7ebe1
陈慧利 <chengwuchao1124@gmail.com>
2022-05-30 09:49:19 +0800

    init layout

---
+++ src/layout/navMenu.vue
@@ -0,0 +1,9 @@
+<template></template>
+
+<script lang="ts">
+import { Vue } from 'vue-class-component';
+
+export default class NavMenu extends Vue {}
+</script>
+
+<style lang="scss" scoped></style>
